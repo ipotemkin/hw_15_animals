bb7aae755a0df37c38649ed82a51cf125814d718
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-10-29 12:24:31 +0300

    added the get_full_record func

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
 from flask import Flask, jsonify, render_template
 from errors import NotFoundError, BadRequestError, ValidationError
-from utils import run_plain_sql, make_results, get_colors_by_animal_id, get_breeds_by_animal_id
+from utils import run_plain_sql, make_results, get_full_record
 
 app = Flask(__name__)
-
-
-SQL = '''
-    select at.animal_type animal_type,
-        name,
-        date_of_birth,
-        outcome_date,
-        o.outcome_subtype,
-        ot.outcome_type
-    from animals_OPT a
-        left join outcome_subtypes o on a.outcome_subtype_id = o.id
-        left join outcome_types ot on a.outcome_type_id = ot.id
-        left join animal_types at on a.animal_type_id = at.id
-    where animal_id = {}
-    '''
 
 
 @app.errorhandler(404)
@@ -43,24 +28,12 @@ def index():
 
 @app.route('/card/<int:uid>')
 def shows_card_by_id(uid: int):
-    if not (results := run_plain_sql(SQL.format(uid))):
-        raise NotFoundError
-    results_with_names = make_results('Animal type', 'Name', 'Date of birth', 'Outcome date', 'Outcome subtype',
-                                      'Outcome type', data=results)
-    results_with_names[0]['Color'] = get_colors_by_animal_id(uid)
-    results_with_names[0]['Breed'] = get_breeds_by_animal_id(uid)
-    return render_template('animal_card.html', card=results_with_names[0])
+    return render_template('animal_card.html', card=get_full_record(uid))
 
 
 @app.route('/<int:uid>')
 def shows_by_id(uid: int):
-    if not (results := run_plain_sql(SQL.format(uid))):
-        raise NotFoundError
-    results_with_names = make_results('Animal type', 'Name', 'Date of birth', 'Outcome date', 'Outcome subtype',
-                                      'Outcome type', data=results)
-    results_with_names[0]['Color'] = get_colors_by_animal_id(uid)
-    results_with_names[0]['Breed'] = get_breeds_by_animal_id(uid)
-    return jsonify(results_with_names[0])
+    return jsonify(get_full_record(uid))
 
 
 @app.route('/breeds/')
